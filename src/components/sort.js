83fa84de24c4be489ed30185d7a3803c7ed81024
Den <fabri.din@yandex.ru>
2020-04-25 14:22:10 +0500

    begin sort

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,5 +1,11 @@
 import AbstractComponent from "./abstract-component.js";
 
+export const SortType = {
+  DATE_DOWN: `date-down`,
+  DATE_UP: `date-up`,
+  DEFAULT: `default`,
+};
+
 const createSortTemplate = () => {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
@@ -7,12 +13,12 @@ const createSortTemplate = () => {
 
       <div class="trip-sort__item  trip-sort__item--event">
         <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-        <label class="trip-sort__btn" for="sort-event">Event</label>
+        <label class="trip-sort__btn" for="sort-event" data-sort-type="${SortType.DEFAULT}">Event</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--time">
         <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-        <label class="trip-sort__btn" for="sort-time">
+        <label class="trip-sort__btn" for="sort-time" data-sort-type="${SortType.DATE_UP}">
           Time
 
         </label>
@@ -20,7 +26,7 @@ const createSortTemplate = () => {
 
       <div class="trip-sort__item  trip-sort__item--price">
         <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">
+        <label class="trip-sort__btn" for="sort-price" data-sort-type="${SortType.DATE_DOWN}">
           Price
           <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
             <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
@@ -34,7 +40,37 @@ const createSortTemplate = () => {
 };
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currenSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  getSortType() {
+    return this._currenSortType;
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (this._currenSortType === sortType) {
+        return;
+      }
+
+      this._currenSortType = sortType;
+
+      handler(this._currenSortType);
+    });
   }
 }
--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,4 +1,4 @@
-import SortComponent from "../components/sort.js";
+import SortComponent, {SortType} from "../components/sort.js";
 import NoPointsComponent from "../components/no-points.js";
 import DaysComponent from "../components/days.js";
 import FormEventComponent from "../components/form-event.js";
@@ -8,6 +8,7 @@ import RouteEditComponent from "../components/route-edit.js";
 import {render, replace, RenderPosition} from "../utils/render.js";
 
 const SHOWING_ROUTE_COUNT_ON_START = 10;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 10;
 
 const renderPoint = (pointListElement, route) => {
   const replacePointToEdit = () => {
@@ -44,6 +45,25 @@ const renderPoint = (pointListElement, route) => {
   render(pointListElement, routeComponent, RenderPosition.BEFOREEND);
 };
 
+const getSortedTasks = (tasks, sortType, from, to) => {
+  let sortedTasks = [];
+  const showingTasks = tasks.slice();
+
+  switch (sortType) {
+    case SortType.DATE_UP:
+      sortedTasks = showingTasks.sort((a, b) => a.dueDate - b.dueDate);
+      break;
+    case SortType.DATE_DOWN:
+      sortedTasks = showingTasks.sort((a, b) => b.dueDate - a.dueDate);
+      break;
+    case SortType.DEFAULT:
+      sortedTasks = showingTasks;
+      break;
+  }
+
+  return sortedTasks.slice(from, to);
+};
+
 export default class TripController {
   constructor(container) {
     this._container = container;
@@ -73,9 +93,24 @@ export default class TripController {
     const pointListElement = container.querySelector(`.trip-events__list`);
 
     let showingRouteCount = SHOWING_ROUTE_COUNT_ON_START;
+
     routes.slice(0, showingRouteCount)
       .forEach((route) => {
         renderPoint(pointListElement, route);
       });
+
+    this._sortComponent.setSortTypeChangeHandler((sortType) => {
+      showingRouteCount = SHOWING_TASKS_COUNT_BY_BUTTON;
+
+      const sortedRoutes = getSortedTasks(routes, sortType, 0, showingRouteCount);
+
+      pointListElement.innerHTML = ``;
+
+      sortedRoutes.slice(0, showingRouteCount)
+        .forEach((task) => {
+          renderPoint(pointListElement, task);
+        });
+
+    });
   }
 }
